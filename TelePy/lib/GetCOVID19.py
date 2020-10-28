79a3e1e12ce5431d1e5ea8c9d783021da54d575c
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-10-28 20:11:19 +0200

    save data in database

--- TelePy/lib/GetCOVID19.py
+++ TelePy/lib/GetCOVID19.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 class GetCOVID19:
     def __init__(self, HOSTNAME, USERNAME, PASSWORD, COVID19API):
         self.__COVID19API = COVID19API
-        self.__get_covid19_info()
+
         self.__db = mysql.connector.connect(
             host=HOSTNAME,
             user=USERNAME,
@@ -18,9 +18,25 @@ class GetCOVID19:
         self.__cursor.execute("USE telepy")
         self.__cursor.execute(
             "CREATE TABLE IF NOT EXISTS Countries (id INT AUTO_INCREMENT PRIMARY KEY, Country VARCHAR(64), CountryCode VARCHAR(3), Slug VARCHAR(128), NewConfirmed INT, TotalConfirmed INT, NewDeaths INT, TotalDeaths INT, NewRecovered INT, TotalRecovered INT, Date VARCHAR(128))")
+        self.__get_covid19_info()
 
     def __get_covid19_info(self):
-        print("Inside")
         responce = requests.get(self.__COVID19API)
         covid_data = responce.json()
-        print("API => ", covid_data)
+
+        for item in covid_data['Countries']:
+            sql = "INSERT INTO Countries (Country, CountryCode, Slug, NewConfirmed, TotalConfirmed, NewDeaths, TotalDeaths, NewRecovered, TotalRecovered, Date) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+            val = (item['Country'], item['CountryCode'], item['Slug'], item['NewConfirmed'], item['TotalConfirmed'],
+                   item['NewDeaths'], item['TotalDeaths'], item['NewRecovered'], item['TotalRecovered'], item['Date'])
+            self.__cursor.execute(sql, val)
+        self.__db.commit()
+
+    def __sort_by_total_confirmed(self):
+        pass
+
+    def __sort_by_new_confirmed(self):
+        pass
+
+    def __sort_by_country_name(self):
+        pass
